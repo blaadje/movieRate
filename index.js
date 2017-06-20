358021f856d4f89841d5334ef48e9d04092da802
blaadje <acharlot91@gmail.com>
2017-06-20 13:29:37 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <h1>Mon application React</h1>,
+  document.getElementById('root')
+)
---
+++ main.js
@@ -0,0 +1,15 @@
+const {app} = require('electron')
+const {BrowserWindow} = require('electron')
+import {enableLiveReload} from 'electron-compile'
+
+enableLiveReload()
+
+app.on('ready', function(){
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        transparent: true,
+        titleBarStyle : 'hidden-inset'
+    })
+    win.loadURL('file://' + __dirname + '/index.html')
+})
