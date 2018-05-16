8ff69937a88f69c5c946a5d81b28f27b0597c494
Alex <alexandre.charlot@wisembly.com>
2018-05-16 14:52:27 +0200

    merging config

--- config/webpack.renderer.config.js
+++ config/webpack.renderer.config.js
@@ -1,76 +1,13 @@
 const path = require('path')
-const { dependencies } = require('../package.json')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const baseWebpackConfig = require('./webpack.config')
+const merge = require('webpack-merge')
 
-let rendererConfig = {
+module.exports = merge(baseWebpackConfig, {
   mode: 'production',
-  entry: {
-    renderer: path.join(__dirname, '../src/index.js')
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json'],
-    alias: {
-      images: path.resolve(__dirname, '../src/assets/images/'),
-      components: path.resolve(__dirname, '../src/components'),
-      containers: path.resolve(__dirname, '../src/containers'),
-      core: path.resolve(__dirname, '../src/core'),
-      settings: path.resolve(__dirname, '../src/settings')
-    }
-  },
-  externals: [
-    ...Object.keys(dependencies || {})
-  ],
   node: {
     __dirname: process.env.NODE_ENV !== 'production',
     __filename: process.env.NODE_ENV !== 'production'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg|jpeg|gif|svg|ttf|eot|svg|woff)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {}
-          }
-        ]
-      },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader'
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          query: { compact: true }
-        }
-      },
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          {
-            loader: 'sass-loader',
-            options: {
-              data: '@import "loader";',
-              includePaths: [
-                path.resolve(__dirname, '../src/assets/style')
-              ]
-            }
-          }
-        ]
-      }
-    ]
   },
   output: {
     filename: '[name].js',
@@ -90,6 +27,4 @@ let rendererConfig = {
     })
   ],
   target: 'electron-renderer'
-}
-
-module.exports = rendererConfig
+})
