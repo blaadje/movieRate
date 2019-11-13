a0f0b96810ce726b9bc4a6e3b914c93fd9d24943
Alexandre Charlot <acharlot91@gmail.com>
2019-11-14 00:03:40 +0100

    fix: tests

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  moduleNameMapper: {
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@images/(.*)$': '<rootDir>/src/assets/images/$1',
+    '^@containers/(.*)$': '<rootDir>/src/containers/$1',
+    '^@core/(.*)$': '<rootDir>/src/core/$1',
+    '^@views/(.*)$': '<rootDir>/src/views/$1',
+    '^@settings/(.*)$': '<rootDir>/src/settings/$1',
+    '.+\\.(css|styl|svg|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
+}
